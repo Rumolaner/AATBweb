db1de95ab64fe7383fabb31652fa251db11787e5
Andreas Nawrath <andreas.nawrath@rumolaner.de>
2023-04-27 15:20:24 +0200

    Grundstruktur jquery

---
+++ perform.php
@@ -0,0 +1,9 @@
+<?php
+
+$test =  "Das ist ein Test";
+
+$data['loggedin'] = false;
+
+echo json_encode($data);
+
+?>
--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-$('footer').html("test");
+$(document).ready(function () {
+  $('footer').text($('footer').text() + new Date().getFullYear().toString());
+
+  Call_Init();
+
+});
+
+class User {
+  constructor(loggedin = false, name = "") {
+    this.loggedin = loggedin;
+    this.name = name;
+  }
+}
+
+function Call_Init() {
+  $.getJSON("perform.php")
+  .done(function (data) {
+    Init(data);
+  })
+  .fail(function () {
+    Protocol("Init could not load JSON");
+  })
+  .always(function () {
+    console.log("always");
+    console.log(user);
+  })
+}
+
+function Protocol(text) {
+  console.log(new Date().toLocaleTimeString("de-DE") + ": " + text);
+}
+
+function Init(data) {
+  console.log("Init");
+  console.log(data);
+
+  if (data['loggedin'] == false) {
+    //Neuer user oder user löschen und login anzeigen
+    user = new User();
+  }
+}

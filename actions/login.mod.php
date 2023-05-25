1062318210683f34796249fb63e3a35123e1ef79
Andreas Nawrath <andreas.nawrath@rumolaner.de>
2023-05-25 16:03:39 +0200

    clsAnswer fertig

--- actions/login.mod.php
+++ actions/login.mod.php
@@ -7,11 +7,15 @@ $password = $param->get('p');
 $user = clsUser::withLogin($mandant, $username, $password);
 if (!$user->active){
   //Fehler zurückgeben
+  $answer->setCOM("Error: ".$trans->get('error2000'));
 } else {
+  //Prüfen ob Account noch Aktiv
+
+
   $id = $user->id;
   $param->setSession('userid', $id);
 }
 
-var_dump($user);
+//var_dump($user);
 
 ?>
